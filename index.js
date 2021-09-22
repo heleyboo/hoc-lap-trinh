662140aed5325f68cab8bcf7f6862728a287513e
Hoan Do Van <vanhoan90it6@gmail.com>
2021-09-22 20:05:54 +0700

    Create http server

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-function batDen() {
-  const den = document.getElementById('den');
-  den.src = 'pic_bulbon.gif'
-  console.log('Bat den')
-}
-function tatDen() {
-  const den = document.getElementById('den');
-  den.src = 'pic_bulboff.gif'
-}
+var http = require('http');
 
-function enter() {
-  const input = document.getElementById('input')
-  const output = document.getElementById('output')
-  output.innerText = input.value
-}
-
-function an() {
-  document.getElementById('output').style.display = 'none'
-}
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8081);
